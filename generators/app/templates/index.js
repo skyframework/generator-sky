d3f42f75ac64c7430493479553bbb95286b0aac4
cristianchaparroa <cristianchaparroa@gmail.com>
2022-07-09 19:00:45 -0500

    exclude node modules

---
+++ generators/app/templates/index.js
@@ -0,0 +1,22 @@
+const Generator = require("yeoman-generator");
+
+module.exports = class extends Generator {
+  constructor(args, opts) {
+    super(args, opts);
+    this.option("name", { type: String, required: true });
+  }
+
+  install() {
+    this.spawnCommand("go", ["mod", "init", this.option.name]);
+    this.spawnCommand("go", ["mod", "tidy"]);
+  }
+
+  writing() {
+    this.log("--> writing");
+    this._writingServiceTemplate();
+  }
+
+  _writingServiceTemplate() {
+    this.fs.copy(this.templatePath("service"), this.destionationPath());
+  }
+};
